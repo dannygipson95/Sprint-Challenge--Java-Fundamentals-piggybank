3f11cd19e0060ab93c0bbd5accccac88039dfad7
Daniel Gipson <dannygipson95@gmail.com>
2020-08-07 13:06:41 -0400

    partially completed stretch

--- piggybank/src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
+++ piggybank/src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,5 +36,39 @@ public class CoinController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    
+    @GetMapping(value = "/money/{amount}", produces = {"application/json"})
+    public ResponseEntity<?> removeAmt(@PathVariable double amount){
+        DecimalFormat hund = new DecimalFormat("#.00");
+        List<Coin> coinList = new ArrayList<>();
+        coinrepos.findAll().iterator().forEachRemaining(coinList::add);
+        double total = 0;
+        for (Coin c : coinList){
+            total += c.getValue()*c.getQuantity();
+        }
+
+        int i = 0;
+        do {
+            if (amount > total) {
+            System.out.println("Money not available");
+            break;
+            } else if (coinList.get(i).getQuantity() > 0 && ((amount*100) - (coinList.get(i).getValue()*100))/100 >= 0){
+                amount = amount - coinList.get(i).getValue();
+                total -= coinList.get(i).getValue();
+                coinList.get(i).rmCoin();
+            } else {
+                i++;
+            }
+        }while (amount != 0.00 && i < 6);
+
+
+        for (Coin c: coinList){
+            if (c.getQuantity() > 1){
+                System.out.println(c.getQuantity() + " " + c.getNameplural());
+            } else {
+                System.out.println(c.getQuantity() + " " + c.getName());
+            }
+        }
+        System.out.println("The piggybank holds " + hund.format(total) + " amount= " + hund.format(amount));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
